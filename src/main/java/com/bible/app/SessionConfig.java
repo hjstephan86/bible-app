bc54247fde4eaf514c0f2c18076111475bb29374
Stephan Epp <hjstephan86@gmail.com>
2024-08-19 20:18:40 +0200

    Use cookies as session tracking mode

---
+++ src/main/java/com/bible/app/SessionConfig.java
@@ -0,0 +1,19 @@
+package com.bible.app;
+
+import java.util.EnumSet;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.servlet.SessionTrackingMode;
+
+@Configuration
+public class SessionConfig {
+    @Bean
+    public ServletContextInitializer initializer() {
+        return servletContext -> {
+            servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+        };
+    }
+}
